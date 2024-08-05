12c4d4a7ee5571c4ca7aaa496fc35dfcf75f2e5f
Mateusz Haldas <mateusz.haldas90@gmail.com>
2024-08-05 21:58:29 +0200

    Adding the lesson number 2  about comparsion. condital instructions (IF, ELIF , ELSE)

---
+++ Lesson2 -  comparison operators, conditional instructions (IF ELSE).py
@@ -0,0 +1,26 @@
+# print(5 == 5) # We will get True or False (Only two options)
+# print(5 != 1) # Or 5 ist diffrent then 1
+# print (5 > 2) # 5 ist bigger then 2
+# print (5 < 1)  # 5 ist less then 1
+# print (5 >= 2) # 5 ist bigger or equal
+# print(5 <= 5 ) # Or 5 ist less or equal then 5?
+#
+# if 5 == 4:
+#     print("I'm the first True!")
+# elif 5 == 5:
+#     print("I'm the second elif! Andd im True!")
+# else:
+#     print("I'm the False!")
+#
+
+x = 4
+y = 15
+
+if x < 2:
+    print("x jest mniejsze niz 2")
+elif x > 2:
+    print("x jest wieksze niz 2 ")
+elif x == 4:
+    print("x jest rowne 4!")
+else:
+    print("Tutaj konczymy Elsem!")
