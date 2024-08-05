908f1f70292dbe369bb73ac42ff2e098095f7670
Mateusz Haldas <mateusz.haldas90@gmail.com>
2024-08-05 19:12:02 +0200

    Second one commit the lesson 1 uploadedgit add .

--- Lesson1 - mathematical operators, type conversion.py
+++ Lesson1 - mathematical operators, type conversion.py
@@ -1,2 +1,30 @@
 print("Kolejnosc!")
-print(2 + 2 * 2)
+print((2 + 2) * 2)  #We are adding only normal brackets to nesting something
+print("Division!")
+print(f"we are reciving here data type FLOAT = {5/2}")
+print("5//2, tutaj dostajemy INT:")
+print(5//2)      # when we are givinh double slash we are reciving here only intiger data type!
+print("Multiply:")
+print(4 * 3)
+print("Root Extraction! :")
+print(3 ** 3)  # Root extraction (kwadrat, potęgi)
+print("Shorterway ::")
+x = 5
+x = x + 1 # Incrementacja
+x += 1  # Skrócona inkrementacja tak lepiej!
+print(x)
+print("Konwersja!:")
+#
+# try:
+#     a = float(input("Podaj liczbe numer 1:  "))
+#     b = float(input ("Podaj liczbe numer 2:"))
+#     print(a + b)
+# except ValueError:
+#     print("Podano zle wartosci sprobuj jeszcze raz!")
+
+y = 2
+z = 2
+print(str(y) + str(z))
+#del y
+print(str(y) + str(z))  # Here we got NameError cuz, with "deL" we got rid of variable y thats why we have here Error!
+
